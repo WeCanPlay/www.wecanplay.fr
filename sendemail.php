c350e5d6b9b147d16fffbd929bb314e7c245d82f
Pierrick Paul <mulotmail@gmail.com>
2012-07-10 16:19:21 +0200

    Form enable

---
+++ sendemail.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_POST)) {
+	if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])) {
+		$msg = "Un message a partir du site www.wecanplay.fr viens d'etre envoyé\n";
+		$msg .= "De la part de ".$_POST['name']." ( ".$_POST['email']." - ".$REMOTE_ADDR." ) \n";
+		$msg .= $_POST['message'];
+		mail("contact@wecanplay.fr", "[form website]", $msg);
+	}
+}
+header("Location: http://www.wecanplay.fr/contact.html");
